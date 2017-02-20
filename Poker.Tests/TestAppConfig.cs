3be62b0c1b674dc4e8727433d56d82d4ccab7fb4
Mogens Heller Grabe <mookid8000@gmail.com>
2017-02-20 14:42:14 +0100

    help find app.config

---
+++ Poker.Tests/TestAppConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace Poker.Tests
+{
+    [TestFixture]
+    public class TestAppConfig
+    {
+        [Test]
+        public void CanGetPath()
+        {
+            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Fake", "FakeApp.config");
+
+            using (Swindler.AppConfig.Use(path))
+            {
+                var pokerPath = AppConfig.GetPath();
+
+                Assert.That(pokerPath, Is.EqualTo(path));
+            }
+        }
+    }
+}
---
+++ Poker/AppConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Poker
+{
+    public static class AppConfig
+    {
+        const string AppConfigFileKey = "APP_CONFIG_FILE";
+
+        /// <summary>
+        /// Gets the path of the app.config of the currently executing process
+        /// </summary>
+        public static string GetPath()
+        {
+            var currentDomain = AppDomain.CurrentDomain;
+            var data = currentDomain.GetData(AppConfigFileKey);
+
+            if (data == null)
+            {
+                throw new ApplicationException($"Could not find '{AppConfigFileKey}' data element in the current appdomain {currentDomain.FriendlyName}");
+            }
+
+            return data.ToString();
+        }
+    }
+}
