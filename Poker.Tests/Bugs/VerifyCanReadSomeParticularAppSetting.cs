b087680429dc9e95dccc5626f69b50ac896ae3ca
Mogens Heller Grabe <mookid8000@gmail.com>
2017-12-12 17:45:57 +0100

    add another testcase

---
+++ Poker.Tests/Bugs/VerifyCanReadSomeParticularAppSetting.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using NUnit.Framework;
+
+namespace Poker.Tests.Bugs
+{
+    [TestFixture]
+    public class VerifyCanReadSomeParticularAppSetting 
+    {
+        [Test]
+        public void ItWorksAsItShould()
+        {
+            var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Bugs", "test01.xml");
+            var xml = File.ReadAllText(filePath, Encoding.UTF8);
+
+            Console.WriteLine(xml);
+
+            var poker = new ConfigurationPoker(xml);
+
+            Assert.That(poker.GetAppSetting("masterkey"), Is.EqualTo("bimmelimmelim"));
+            Assert.That(poker.GetAppSetting("logpath"), Is.EqualTo(@"c:\data\fm5"));
+        }
+    }
+}
